45756dd7557bc277dbb0cd24d7276b2868a0e354
Ana Ribeiro <analidialirar@gmail.com>
2022-04-24 12:21:17 -0300

    feat: add book mocked database

---
+++ data/database.js
@@ -0,0 +1,81 @@
+const books = [
+  {
+    name: 'Algoritmos para viver',
+    genre: 'tecnologia',
+    pages: 500,
+    recommend: true,
+    readed: true
+  },
+  {
+    name: 'Algoritmos de destruição em massa',
+    genre: 'tecnologia',
+    pages: 300,
+    recommend: true,
+    read: true
+  },
+  {
+    name: 'Estrutura de dados e algoritmos com JavaScript',
+    genre: 'tecnologia',
+    pages: 350,
+    recommend: true,
+    read: true
+  },
+  {
+    name: 'O Hobbit',
+    genre: 'fantasia',
+    pages: 285,
+    recommend: false,
+    read: false
+  },
+  {
+    name: 'Objetos Cortantes',
+    genre: 'suspense psicológico',
+    pages: 327,
+    recommend: false,
+    read: false,
+  },
+  {
+    name: 'Senhor dos Aneis',
+    genre: 'fantasia',
+    pages: 326,
+    recommend: false,
+    read: false
+  },
+  {
+    name: 'Torto Arado',
+    genre: 'romance',
+    pages: 245,
+    recommend: true,
+    read: true
+  },
+  {
+    name: 'Dororidade',
+    genre: 'feminismo',
+    pages: 98,
+    recommend: true,
+    read: true
+  },
+  {
+    name: 'Um exu em Nova York',
+    genre: 'contos',
+    pages: 120,
+    recommend: true,
+    read: true
+  },
+  {
+    name: 'Para todas as pessoas intensas',
+    genre: 'fantasia',
+    pages: 150,
+    recommend: false,
+    read: false
+  },
+  {
+    name: 'Your name',
+    genre: 'mangá',
+    pages: 400,
+    recommend: true,
+    read: true
+  }
+]
+
+module.exports = books
